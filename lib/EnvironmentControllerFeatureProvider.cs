656dd6be100e62f101d1bc44b743428e2b49dd33
Homayoun Behzadian <6245340+behzadian@users.noreply.github.com>
2026-06-14 12:36:10 +0200

    Standard & nuget publishing applied (#3)

--- lib/EnvironmentControllerFeatureProvider.cs
+++ lib/EnvironmentControllerFeatureProvider.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Hosting;
